c5b7bc67ad970bfe90f8f5ad17f397ad15ad2ebd
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2021-12-23 23:25:49 -0600

    Add files via upload

---
+++ positve.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int p;
+	printf("enter the p");
+	scanf("%d",&p);
+	if(p<0)
+	{
+		printf("it is a negitive");
+	}
+	else 
+	{
+		printf( "it is a positive" );
+	}
+}

e7c8bd2062dec442be58a61ee452e9e1c5ec730f
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2021-12-24 00:10:11 -0600

    Add files via upload

---
+++ COST.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int cp,sp;
+	printf("enter the cp and sp");
+	scanf("%d%d",&cp,&sp);
+	if(cp>sp)
+	{
+		printf("LOSS");
+	}
+    else 
+    {
+    	printf("PROFIT");
+	}
+}

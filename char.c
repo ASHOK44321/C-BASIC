fda60e4c2c0a612b1597bfd4133ffd7fcebc3ad6
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2021-12-23 23:29:49 -0600

    Add files via upload

---
+++ char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("\n read 3 values");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("\n a=%d,b=%d,c=%d",a,b,c);
+}
+ a b c d e f g h i j k l m n  o p q r s t u v w x y z
+ 

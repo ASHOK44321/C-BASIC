f7094f15314bd1a0283e3c5d9587230173b65419
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2021-12-30 11:56:08 +0530

    Add files via upload

---
+++ fail.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("enter the marks");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+    if (a>=35)
+    {
+    	printf("\nPASS");
+	}
+	else
+	{
+		printf("\nFAIL");
+	}
+	 if (b>=35)
+    {
+    	printf("\nPASS");
+	}
+	else
+	{
+		printf("\nFAIL");
+	}
+	 if (c>=35)
+    {
+    	printf("\nPASS");
+	}
+	else
+	{
+		printf("\nFAIL");
+	}
+	 if (d>=35)
+    {
+    	printf("\nPASS");
+	}
+	else
+	{
+		printf("\nFAIL");
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	

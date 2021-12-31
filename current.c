1d9508af899ebf1ebf15305c3ec4065eda5a55f5
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2021-12-31 10:48:07 +0530

    Add files via upload

---
+++ current.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+{
+	int unit;
+	float amt,surcharg,na;
+	printf("enter the units");
+	scanf("%d",&unit);
+	if (unit<200)
+	{
+		amt=unit*1.20;
+		printf("amount=%f",amt);
+	}
+	else if (unit>=200 && unit<400)
+	{
+		amt=unit*1.50;
+		printf("amount=%f",amt);
+	}
+	else if (unit>=400 && unit<600)
+	{
+		amt=unit*1.80;
+		printf("amount=%f",amt);
+		
+	}
+	else 
+	{
+		amt=unit*2.00;
+		printf("amount=%f",amt);
+	}
+	if(amt>400)
+	{
+	surcharg=amt*15/100;
+	printf("\nSurcharge=%f",surcharg);
+	na=amt+surcharg;
+	printf("\nNet amount=%f",na);
+    }
+}
+

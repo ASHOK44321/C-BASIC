f6691fe62a25218dd57bdebfb3ad97175089edfd
ASHOK44321 <96563901+ASHOK44321@users.noreply.github.com>
2022-01-07 12:02:27 +0530

    Add files via upload

---
+++ dc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+ {
+ 	int n,r,sum=0;
+ 	scanf("%d", &n);
+ 	while(n>0)
+ 	{
+ 		r=n%10;
+		 printf("%d\n", r);
+		 n= n/10;
+		 sum += r;
+	 }
+	 printf("______ ");
+	 printf("\nsum=%d",sum);
+	 printf("\n______");
+}
